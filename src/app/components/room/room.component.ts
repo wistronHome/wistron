5c79ad748bc1eb8c43b669be89abbddebf9d0b6c
追逐萌萌的你 <1216621881@qq.com>
2017-10-11 11:28:43 +0800

    改拖拽

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -72,14 +72,15 @@ import {
 export class RoomComponent implements OnInit {
     state: string = 'active';
     state1: string = 'inactive';
-    model="";
+    model = '';
     id = '';
-    ChildId='';
+    ChildId = '';
+
     constructor() {
     }
 
     ngOnInit() {
-        var Q = window['Q']
+        var Q = window['Q'];
         var graph = new Q.Graph('mcRoom');
         //设置坐标原点
         graph.originAtCenter = false;
@@ -91,7 +92,7 @@ export class RoomComponent implements OnInit {
             row.moveTo(0, height);
             row.lineTo(780, height);
             row.setStyle(Q.Styles.SHAPE_STROKE_STYLE, 'black');
-            row.setStyle(Q.Styles.SHAPE_STROKE, 0.5)
+            row.setStyle(Q.Styles.SHAPE_STROKE, 0.5);
             row.setStyle(Q.Styles.SHAPE_LINE_DASH, [5, 2]);
             row.isSelected = function () {
                 return false;
@@ -99,7 +100,7 @@ export class RoomComponent implements OnInit {
             row.isMovable = function () {
                 return false;
             };
-            if (i % 4 == 0) {
+            if (i % 4 === 0) {
                 row.setStyle(Q.Styles.SHAPE_LINE_DASH, [5, 0]);
             }
         }
@@ -107,8 +108,8 @@ export class RoomComponent implements OnInit {
         for (var i = 0; i < 53; i++) {
             var line = graph.createShapeNode();
             var width = i * 15;
-            line.moveTo(width, 0)
-            line.lineTo(width, 600)
+            line.moveTo(width, 0);
+            line.lineTo(width, 600);
             line.setStyle(Q.Styles.SHAPE_STROKE_STYLE, 'black');
             line.setStyle(Q.Styles.SHAPE_LINE_DASH, [5, 2]);
             if (i % 4 == 0) {
@@ -117,88 +118,84 @@ export class RoomComponent implements OnInit {
             }
         }
         //设置是否能被选择
-        graph.isSelectable =  (item)=> {
+        graph.isSelectable = (item) => {
             // console.log(item);
             return item.name === "computer";
         };
-        graph.editable = (item)=> {
+        graph.editable = (item) => {
             console.log(item);
             return item.name === "computer";
         }
         //鼠标按下之后记录按下的信息
         var src, startX, startY;
-        document.onmousedown =  (e)=> {
+        document.ondragstart = (e) => {
             console.log(e.target['src']);
-            
-               src= e.target['src'] && e.target['src'].includes("svg") ? e.target['src'].split("/")[4] : 'aaaa';
-                // src = e.target['src'].split("/")[4];
-                startX = e.offsetX;
-                startY = e.offsetY;
-            
-            
+            src = e.target['src'] && e.target['src'].includes("svg") ? e.target['src'].split("/")[4] : 'aaaa';
+            // src = e.target['src'].split("/")[4];
+            startX = e.offsetX;
+            startY = e.offsetY;
         }
-        document.onmousemove =  (e)=> {
+        document.onmousemove = (e) => {
 
 
         }
-        document.ondrop = (e)=> {
-            var p = graph.toLogical(e.offsetX, e.offsetY)
-            var computer = graph.createNode("computer", p.x, p.y);
+        document.ondrop = (e) => {
+            var p = graph.toLogical(e.offsetX, e.offsetY);
+            var computer = graph.createNode('computer', p.x, p.y);
             let image = 'assets/' + src;
             console.log(image);
-        
             computer.image = image;
-            computer.size = {width: 60 }
-            var alarmUI = graph.createNode( '' ,p.x+30,p.y-30)
+            computer.size = {width: 60};
+            var alarmUI = graph.createNode('', p.x + 30, p.y - 30);
             alarmUI.image = 'assets/alarmred.svg';
-            alarmUI.size = { width: 30 }
-            alarmUI.zIndex=999;
+            alarmUI.size = {width: 30};
+            alarmUI.zIndex = 999;
             alarmUI.host = computer;
             alarmUI.parent = computer;
             //数据的存储
-            var arr = [];
-            model.forEach( (node)=> {
-                if (node.$name === "computer") {
-                    var element = {name: '', id: '', x: '', y: ''}
-                    element.name = node.name
+            let arr = [];
+            model.forEach((node) => {
+                if (node.$name === 'computer') {
+                    var element = {name: '', id: '', x: '', y: ''};
+                    element.name = node.name;
                     element.id = node.id;
                     element.x = node.x;
                     element.y = node.y;
-                    arr.push(element)
+                    arr.push(element);
                     console.log(arr);
                 }
             });
-        }
+        };
 
 
-        var model = graph.graphModel; 
-        this.model=model;
+        var model = graph.graphModel;
+        this.model = model;
         //数据的渲染
-        var info = [{name: 'jjj', id: '1', x: 200, y: 200}, {name: 'ppp', id: '2', x: 300, y: 300}]
+        let info = [{name: 'jjj', id: '1', x: 200, y: 200}, {name: 'ppp', id: '2', x: 300, y: 300}];
         for (var i = 0; i < info.length; i++) {
-            graph.createNode(info[i].name, info[i].x, info[i].y)
+            graph.createNode(info[i].name, info[i].x, info[i].y);
         }
         //点击机房
         graph.onclick = e => {
             // console.log(e);
             // console.log(e.getUI());
             // console.log(e.getData().type);
-            if( e.getData()&&e.getData().type =="Q.Node"){
+            if (e.getData() && e.getData().type == "Q.Node") {
                 this.id = e.getData().id;
                 //子图元的id
-                if(e.getData().data){
-                this.ChildId = e.getData().data.children.datas[0].id;
+                if (e.getData().data) {
+                    this.ChildId = e.getData().data.children.datas[0].id;
                 }
-            this.state = 'active';
+                this.state = 'active';
             }
-            
-        }
+        };
+    }
 
-    }
     delCabinet(): void {
-        this.model['removeById'](this.id)
-        this.model['removeById'](this.ChildId)
+        this.model['removeById'](this.id);
+        this.model['removeById'](this.ChildId);
     }
+
     toggle(): void {
         if (this.state === 'active') {
             this.state = 'inactive';
@@ -208,7 +205,8 @@ export class RoomComponent implements OnInit {
             this.state1 = 'inactive';
         }
     }
-    toolsToggle():void{
+
+    toolsToggle(): void {
         if (this.state1 === 'active') {
             // this.state = 'active';
             this.state1 = 'inactive';
